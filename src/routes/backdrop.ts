93bc8b8fe6b62208d919d3940a9cc9cbd9db4aa1
mjrdd <enigmaticjayar@gmail.com>
2023-05-13 13:18:20 +0800

    Improve clock hands position calculation

--- src/routes/backdrop.ts
+++ src/routes/backdrop.ts
@@ -27,7 +27,7 @@ export function createBackdrop(canvas: HTMLCanvasElement) {
 	const ctx = new Graphics();
 
 	function drawClock() {
-		ctx.lineStyle(2, 0x020202, 1);
+		ctx.lineStyle(2, 0x454545, 1);
 
 		for (let i = 0; i < 12; i++) {
 			ctx.moveTo(
@@ -45,25 +45,35 @@ export function createBackdrop(canvas: HTMLCanvasElement) {
 		const min = time.getMinutes();
 		const hrs = time.getHours();
 
-		ctx.lineStyle(5, 0x222222, 1);
-		ctx.moveTo(clock.position.x, clock.position.y);
+		ctx.lineStyle(8, 0x222222, 1);
+		ctx.moveTo(
+			clock.position.x +
+				0.08 * clock.radius * Math.cos(-(Math.PI / 3) * (hrs + min / 60 - 3)),
+			clock.position.y + 0.08 * clock.radius * Math.sin(-(Math.PI / 3) * (hrs + min / 60 - 3))
+		);
 		ctx.lineTo(
-			clock.position.x + 0.5 * clock.radius * Math.cos(Math.PI * (hrs / 6) - Math.PI * 0.5),
-			clock.position.y + 0.5 * clock.radius * Math.sin(Math.PI * (hrs / 6) - Math.PI * 0.5)
+			clock.position.x + 0.5 * clock.radius * Math.cos(-(Math.PI / 3) * (hrs + min / 60)),
+			clock.position.y + 0.5 * clock.radius * Math.sin(-(Math.PI / 3) * (hrs + min / 60))
 		);
 
-		ctx.lineStyle(3, 0x222222, 1);
-		ctx.moveTo(clock.position.x, clock.position.y);
+		ctx.lineStyle(5, 0x222222, 1);
+		ctx.moveTo(
+			clock.position.x + 0.1 * clock.radius * Math.cos(0.5 * Math.PI * (min / 15 - 3)),
+			clock.position.y + 0.1 * clock.radius * Math.sin(0.5 * Math.PI * (min / 15 - 3))
+		);
 		ctx.lineTo(
-			clock.position.x + 0.75 * clock.radius * Math.cos(Math.PI * (min / 30) - Math.PI * 0.5),
-			clock.position.y + 0.75 * clock.radius * Math.sin(Math.PI * (min / 30) - Math.PI * 0.5)
+			clock.position.x + 0.75 * clock.radius * Math.cos(0.5 * Math.PI * (min / 15 - 1)),
+			clock.position.y + 0.75 * clock.radius * Math.sin(0.5 * Math.PI * (min / 15 - 1))
 		);
 
 		ctx.lineStyle(2, 0xff2125, 1);
-		ctx.moveTo(clock.position.x, clock.position.y);
+		ctx.moveTo(
+			clock.position.x + 0.15 * clock.radius * Math.cos(0.5 * Math.PI * (sec / 15 - 3)),
+			clock.position.y + 0.15 * clock.radius * Math.sin(0.5 * Math.PI * (sec / 15 - 3))
+		);
 		ctx.lineTo(
-			clock.position.x + 0.75 * clock.radius * Math.cos(Math.PI * (sec / 30) - Math.PI * 0.5),
-			clock.position.y + 0.75 * clock.radius * Math.sin(Math.PI * (sec / 30) - Math.PI * 0.5)
+			clock.position.x + 0.95 * clock.radius * Math.cos(0.5 * Math.PI * (sec / 15 - 1)),
+			clock.position.y + 0.95 * clock.radius * Math.sin(0.5 * Math.PI * (sec / 15 - 1))
 		);
 	}
 
