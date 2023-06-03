c0d1260795a6c22b5948504cfe1a7f574295213c
mjrdd <enigmaticjayar@gmail.com>
2023-06-03 19:14:09 +0800

    Add url expiration check and specific error fallback

--- src/routes/l/[slug]/+page.server.ts
+++ src/routes/l/[slug]/+page.server.ts
@@ -1,25 +1,36 @@
-import { error, fail, redirect } from "@sveltejs/kit";
+import { error, redirect } from "@sveltejs/kit";
 import { ClientResponseError } from "pocketbase";
+import { isDate, isPast, toDate } from "date-fns";
 
 export async function load({ locals, params, url }) {
 	let record;
 
 	try {
 		record = await locals.pb
-			.collection("url-shortener")
+			.collection("url_shortener")
 			.getFirstListItem(`key="${params.slug}"`);
 	} catch (err) {
 		if (err instanceof ClientResponseError) {
-			return fail(400);
+			throw error(404, {
+				message:
+					"The requested information could not be found in the database. Please verify the link and if the error persists, kindly contact the administrator for further assistance."
+			});
 		}
 
-		throw error(500);
+		throw error(500, { message: "An unexpected error has occurred. Please try again later." });
 	}
 
 	const showStat = Boolean(url.searchParams.get("stat"));
 
 	if (record && !showStat) {
-		throw redirect(303, record.link);
+		if (record.expiration && isDate(record.expiration) && isPast(toDate(record.expiration))) {
+			throw error(400, {
+				message:
+					"The link you are trying to access has expired. Kindly reach out to the provider of the link for further assistance."
+			});
+		}
+
+		throw redirect(303, record.url);
 	}
 
 	return {
