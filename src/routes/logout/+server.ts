16eee7cfb9be1276cf61d6040a1cf80e40539941
mjrdd <enigmaticjayar@gmail.com>
2023-06-24 00:04:34 +0800

    Update logout api

--- src/routes/logout/+server.ts
+++ src/routes/logout/+server.ts
@@ -3,10 +3,8 @@ import { redirect } from "@sveltejs/kit";
 export async function POST({ locals, url }) {
 	locals.pb.authStore.clear();
 
-	const params = Object.fromEntries(url.searchParams.entries());
-	if (params.redirectTo) {
-		throw redirect(303, params.redirectTo);
+	if (url.searchParams.has("redirectTo")) {
+		throw redirect(303, url.searchParams.get("redirectTo") || "/");
 	}
-
-	return new Response();
+	return new Response(undefined, { status: 200 });
 }
