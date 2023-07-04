5136fde26603ec7cc2ba356144f430adfb6052c6
mjrdd <enigmaticjayar@gmail.com>
2023-07-04 22:12:01 +0800

    Update login design

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,7 +2,14 @@
 	import { browser } from "$app/environment";
 	import { superForm } from "sveltekit-superforms/client";
 
-	import { ProgressRadial, toastStore, type ToastSettings } from "@skeletonlabs/skeleton";
+	import {
+		AppShell,
+		AppBar,
+		LightSwitch,
+		ProgressRadial,
+		toastStore,
+		type ToastSettings
+	} from "@skeletonlabs/skeleton";
 
 	import Icon from "@iconify/svelte/offline";
 	import biAsterisk from "@iconify-icons/bi/asterisk";
@@ -41,47 +48,71 @@
 	}
 </script>
 
-<div class="flex h-screen items-center justify-center">
-	<form method="POST" use:enhance class="m-2 w-full max-w-lg p-2">
-		<div class="h2">Account Login</div>
-		<div class="input-group input-group-divider mt-8 grid-cols-[auto_1fr_auto] !bg-transparent">
-			<div class="input-group-shim">
-				<Icon icon={biEnvelopeAt} />
+<svelte:head>
+	<title>Login to mjayar</title>
+</svelte:head>
+
+<AppShell
+	class="bg-gradient-to-br from-surface-200 from-10% to-surface-50 to-45% dark:from-surface-800 dark:to-surface-900">
+	<svelte:fragment slot="header">
+		<AppBar background="bg-transparent">
+			<svelte:fragment slot="lead">
+				<div>
+					<a href="/" class="uppercase font-extrabold">mjayar</a>
+				</div>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<div>
+					<LightSwitch />
+				</div>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+
+	<div class="relative flex h-full items-center justify-center">
+		<form method="POST" use:enhance class="m-2 w-full max-w-md p-2">
+			<div class="h2">Account Login</div>
+			<div
+				class="input-group input-group-divider mt-8 grid-cols-[auto_1fr_auto] !bg-transparent">
+				<div class="input-group-shim">
+					<Icon icon={biEnvelopeAt} />
+				</div>
+				<input
+					type="text"
+					name="email_or_username"
+					placeholder="Email address or username"
+					bind:value={$login_form.email_or_username}
+					{...$constraints.email_or_username} />
 			</div>
-			<input
-				type="text"
-				name="email_or_username"
-				placeholder="Email address or username"
-				bind:value={$login_form.email_or_username}
-				{...$constraints.email_or_username} />
-		</div>
-		{#if $errors.email_or_username}
-			<span class="text-sm text-red-600">{$errors.email_or_username}</span>
-		{/if}
+			{#if $errors.email_or_username}
+				<span class="text-sm text-red-600">{$errors.email_or_username}</span>
+			{/if}
 
-		<div class="input-group input-group-divider mt-6 grid-cols-[auto_1fr_auto] !bg-transparent">
-			<div class="input-group-shim">
-				<Icon icon={biAsterisk} />
+			<div
+				class="input-group input-group-divider mt-6 grid-cols-[auto_1fr_auto] !bg-transparent">
+				<div class="input-group-shim">
+					<Icon icon={biAsterisk} />
+				</div>
+				<input
+					type="password"
+					name="password"
+					placeholder="Password"
+					{...$constraints.password} />
 			</div>
-			<input
-				type="password"
-				name="password"
-				placeholder="Password"
-				{...$constraints.password} />
-		</div>
-		{#if $errors.password}
-			<span class="text-sm text-red-600">{$errors.password}</span>
+			{#if $errors.password}
+				<span class="text-sm text-red-600">{$errors.password}</span>
+			{/if}
+
+			<button type="submit" class="btn variant-filled-primary mt-6 w-full">
+				<span class="font-extrabold uppercase">Login</span>
+			</button>
+		</form>
+
+		{#if $submitting}
+			<div
+				class="fixed top-0 flex h-full w-full items-center justify-center bg-white !bg-opacity-80 dark:bg-black">
+				<div><ProgressRadial value={undefined} /></div>
+			</div>
 		{/if}
-
-		<button type="submit" class="btn variant-filled-primary mt-6 w-full">
-			<span class="font-extrabold uppercase">Login</span>
-		</button>
-	</form>
-</div>
-
-{#if $submitting}
-	<div
-		class="fixed top-0 flex h-screen w-screen items-center justify-center bg-white !bg-opacity-80 dark:bg-black">
-		<div><ProgressRadial value={undefined} /></div>
 	</div>
-{/if}
+</AppShell>
