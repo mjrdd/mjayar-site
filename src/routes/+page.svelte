c2c047abcdd655233e87d290fc3a1997d1c5b7d8
mjrdd <enigmaticjayar@gmail.com>
2023-07-04 22:04:16 +0800

    Update homepage design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-	import FunctionPlot from "$lib/components/FunctionPlot.svelte";
-	import Katex from "$lib/components/Katex.svelte";
-	import { LightSwitch } from "@skeletonlabs/skeleton";
+	import { AppBar, AppShell, LightSwitch } from "@skeletonlabs/skeleton";
 </script>
 
 <svelte:head>
@@ -9,23 +7,30 @@
 	<meta name="description" content="Welcome to mjayar's personal website" />
 </svelte:head>
 
-<div
+<AppShell
 	class="bg-gradient-to-br from-surface-200 from-10% to-surface-50 to-45% dark:from-surface-800 dark:to-surface-900">
-	<header class="flex h-16 items-center justify-end px-8">
-		<LightSwitch rounded="rounded-full" />
-	</header>
-	<div
-		class="container grid h-[640px] w-full grid-cols-none grid-rows-2 md:grid-cols-2 md:grid-rows-none md:items-center">
-		<div class="m-2 overflow-hidden">
-			<Katex class="flex justify-center">f(x) = x^2</Katex>
-			<FunctionPlot options={{ data: [{ fn: "x^2" }], disableZoom: true, grid: true }} />
-		</div>
-		<div class="mx-8 flex flex-col justify-center gap-8 md:mx-16">
-			<h1 class="font-extrabold">Welcome to mjayar!</h1>
+	<svelte:fragment slot="header">
+		<AppBar background="bg-transparent">
+			<svelte:fragment slot="lead">
+				<div>
+					<a href="/" class="uppercase font-extrabold">mjayar</a>
+				</div>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<div>
+					<LightSwitch />
+				</div>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+
+	<div class="flex h-full justify-center items-center p-6 md:p-10">
+		<div class="max-w-md text-center">
+			<h1 class="font-extrabold mb-8 md:mb-12">Welcome to mjayar!</h1>
 			<p class="font-extralight leading-loose">
 				Discover a captivating world of creativity and innovation on my personal website,
 				where I showcase a carefully curated collection of my extraordinary projects.
 			</p>
 		</div>
 	</div>
-</div>
+</AppShell>
