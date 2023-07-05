c86118459cfcda3ea1d44dcf8b96a241737564ab
mjrdd <enigmaticjayar@gmail.com>
2023-07-05 12:39:37 +0800

    Switch to afterupdate

--- src/lib/components/FunctionPlot.svelte
+++ src/lib/components/FunctionPlot.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
-	import { onMount } from "svelte";
+	import { afterUpdate } from "svelte";
 	import { twClass } from "../utils";
 	import type { FunctionPlotOptions } from "function-plot";
 
-	export let className = "";
-
 	export let element = "div";
 	export let options: Omit<FunctionPlotOptions, "target"> = {};
+
+	let className = "";
 	export { className as class };
 
-	let target: HTMLElement;
+	let plotContainer: HTMLElement;
 
-	onMount(async () => {
+	afterUpdate(async () => {
 		const functionPlot = (await import("function-plot")).default;
-		functionPlot({ target, ...options });
+		functionPlot({ target: plotContainer, ...options });
 	});
 </script>
 
 <svelte:element
 	this={element}
-	bind:this={target}
+	bind:this={plotContainer}
 	class={twClass(
 		"dark:[&_.x.axis_.tick_line]:stroke-white dark:[&_.x.origin]:stroke-white dark:[&_.y.axis_.tick_line]:stroke-white dark:[&_.y.origin]:stroke-white",
 		className
