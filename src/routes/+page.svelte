d4f43ab38d2aa45b97097305b6549f0ac97caded
mjrdd <enigmaticjayar@gmail.com>
2024-01-08 01:54:58 +0800

    Add ui components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-    import { Button } from '$ui/button';
     import { readable } from 'svelte/store';
     import { Sun, Moon } from 'lucide-svelte';
     import { toggleMode } from 'mode-watcher';
+    import { Button } from '$ui/button';
+    import * as Tooltip from '$ui/tooltip';
 
     let dateStore = readable(new Date(), (set) => {
         const interval = setInterval(() => {
@@ -11,6 +12,18 @@
 
         return () => clearInterval(interval);
     });
+
+    const BIRTHDATE = new Date(2000, 8, 5);
+
+    function getAge() {
+        let today = new Date();
+        let age = today.getFullYear() - BIRTHDATE.getFullYear();
+        let monthDiff = today.getMonth() - BIRTHDATE.getMonth();
+        if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < BIRTHDATE.getDate())) {
+            age -= 1;
+        }
+        return age;
+    }
 </script>
 
 <svelte:head>
@@ -18,9 +31,17 @@
 </svelte:head>
 
 <div class="flex justify-end space-x-2 p-4">
-    <Button variant="ghost">
-        {Intl.DateTimeFormat('en-us', { timeStyle: 'medium' }).format($dateStore)}
-    </Button>
+    <Tooltip.Root>
+        <Tooltip.Trigger asChild let:builder>
+            <Button builders={[builder]} variant="ghost">
+                {Intl.DateTimeFormat('en-us', { timeStyle: 'medium' }).format($dateStore)}
+            </Button>
+        </Tooltip.Trigger>
+        <Tooltip.Content>
+            <p>{Intl.DateTimeFormat('en-us', { dateStyle: 'full' }).format($dateStore)}</p>
+        </Tooltip.Content>
+    </Tooltip.Root>
+
     <Button on:click={toggleMode} variant="outline" size="icon">
         <Sun
             class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
@@ -33,6 +54,11 @@
 </div>
 
 <div class="content-grid space-y-4">
-    <h1 class="text-2xl">Hi!</h1>
-    <p>I'm Jay Ar.</p>
+    <h1 class="text-2xl">Hello!</h1>
+    <div class="leading-8">
+        <p>
+            I'm Jay-ar, a {getAge()}-year-old civil engineering student from the Philippines.
+        </p>
+        <p>I enjoy watching anime, playing video games, and programming.</p>
+    </div>
 </div>
